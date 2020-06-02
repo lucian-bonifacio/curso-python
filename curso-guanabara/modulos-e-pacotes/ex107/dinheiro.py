442c86fe02b9411eb2afbf9687ffe74d701ba34c
lucian-bonifacio <56202696+lucian-bonifacio@users.noreply.github.com>
2020-06-02 13:58:58 -0300

    exercicio 107 guanabara

---
+++ curso-guanabara/modulos-e-pacotes/ex107/dinheiro.py
@@ -0,0 +1,8 @@
+import moeda
+
+
+preco = float(input('Digite um preço: R$'))
+print(moeda.aumentar(preco))
+print(moeda.dobro(preco))
+print(moeda.metade(preco))
+
---
+++ curso-guanabara/modulos-e-pacotes/ex107/moeda.py
@@ -0,0 +1,11 @@
+def aumentar(num):
+    aum = (num * 0.1) + num 
+    return f'Aumentando 10%, temos R${aum}'
+
+
+def dobro(num):
+    return f'O dobro de R${num} é R${num*2}'
+
+
+def metade(num):
+    return f'A metade de R${num} é R${num/2}'
